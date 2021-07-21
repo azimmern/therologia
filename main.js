f257e09d903b230e8ed2cad36a5accd423d51bb0
lePetitProf <azimmern@users.noreply.github.com>
2021-07-21 17:40:17 -0400

    Added main html, css, and js files
    Also added TheroHTMLtest(1).html, which is the html doc generated from the xslt that Dan and I built. The learning goal should be to have the xslt build out the html page directly into its final form; the likely goal will be to end up with a finished html product, irrespective of the xml/xslt upstream of it.

---
+++ main.js
@@ -0,0 +1,31 @@
+$(document).ready( function(){
+
+// cypher and decypher buttons
+
+$("#togglecypherbutton").on("click", function(){
+    if(  $(".decypher").hasClass("d-none")) {
+
+        $(".decypher").removeClass("d-none").css("display", "inline");
+    }else {
+        $(".decypher").addClass("d-none").css("display", "none");
+    }
+    
+
+    
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+});
+// document ready
