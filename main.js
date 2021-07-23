b77dbc8a4ea977cfe15c11625abef3bf0f9dd72c
lePetitProf <azimmern@users.noreply.github.com>
2021-07-23 18:31:15 -0400

    click on cypher, options for popovers with jscript objects

--- main.js
+++ main.js
@@ -1,31 +1,50 @@
 $(document).ready( function(){
 
-// cypher and decypher buttons
+ // Show all and hide all
+ $("#toggle-cypher-button").on("click", function() {
 
-$("#togglecypherbutton").on("click", function(){
-    if(  $(".decypher").hasClass("d-none")) {
-
-        $(".decypher").removeClass("d-none").css("display", "inline");
-    }else {
-        $(".decypher").addClass("d-none").css("display", "none");
+    // if anything with class of "decypher" is invisible...
+    if ($(".decypher").hasClass("d-none")) {
+      // show all the things with class of decypher
+      $(".decypher").removeClass("d-none").css("display", "inline");
+      // change the text in the button to hide all
+      $("#toggle-cypher-button").text("Hide all");
+    } else {
+      // Hide all the things with class of decypher
+      $(".decypher").addClass("d-none").css("display", "none");
+      // change the text in the button to hide all
+      $("#toggle-cypher-button").text("Show all");
     }
-    
 
-    
-
-
-});
+  });
 
 
+  // CLicking on individual cypher classes
+  $(".cypher").on("click", function() {
 
+    if ($(this).next().hasClass("d-none")) {
+      $(this).next().fadeIn("fast").removeClass("d-none").css("display", "inline");
+    } else {
+      $(this).next().fadeOut("fast", function() {
+        $(this).addClass("d-none").css("display", "none");
+      });
+    }
 
+  });
 
+  // Set options for popovers per page with javascript objects
+  $('[data-toggle="popover"]').popover({
+    'container': 'body',
+    "animation": true,
+    "html": true,
+    "placement": 'top',
+    "trigger": 'click'
+  });
 
-
-
-
-
-
+  // Use this to allow popover to dismiss on clicking any element
+  $('.popover-dismiss').popover({
+    trigger: 'focus'
+  });
 
 });
 // document ready
