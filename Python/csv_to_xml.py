4fadd4bdd57dc7be54ce2a60df89664fd90d3363
lePetitProf <azimmern@users.noreply.github.com>
2022-04-11 19:19:37 -0400

    updated footnotes 2022_04_11

--- Python/csv_to_xml.py
+++ Python/csv_to_xml.py
@@ -1,6 +1,7 @@
 import csv
 
-f = open('ref_extract_with_notes_test.csv')
+#Update this file name with the latest file version, e.g. ref_extract_2025_07_11
+f = open("ref_extract_2022_04_11.csv")
 csv_f = csv.reader(f)   
 data = []
 
@@ -8,13 +9,11 @@ for row in csv_f:
 	data.append(row)
 f.close()
 
-#print (data[1:])
+#defines a function convert_row(row) that takes csv data and pushes it to an xml <note>
 
 def convert_row(row):
     return """<note type="editorial" xml:id="%s" resp="%s">%s</note>\n""" % (row[0], row[4], row[3])
 
+#overwrites the file xml_notes.xm, converting each row into a separate <note>
 output_file = open('xml_notes.xml', 'w')
 output_file.writelines(convert_row(L) for L in data[2:])
-
-
-#print ('\n'.join([ convert_row(row) for row in data[1:]]))
